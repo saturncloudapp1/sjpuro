cbcc1d85734644ae17e2ebb50e8d8416dcf6e08d
saturncloudapp1 <149740534+saturncloudapp1@users.noreply.github.com>
2023-11-09 19:40:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.url === '/hello' && req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello, World!\n');
